07c36f27a6c3c6d97ef81a209b1ab3da9b40c12e
Stephen Mahon <stemahon@gmail.com>
2022-03-17 23:35:49 -0500

    implemented recursive backtracker

--- 7-maze-generator/cell.js
+++ 7-maze-generator/cell.js
@@ -4,6 +4,51 @@ class Cell {
         this.j = j;
         // top, right, bottom, left
         this.walls = [true, true, true, true];
+        this.visited = false;
+    }
+
+    index(i, j) {
+        if (i < 0 || j < 0 || i > cols-1 || j > rows-1) {
+            return -1
+        }
+        return i + j * cols;
+    }
+
+    checkNeighbors() {
+        let neighbors = [];
+
+        let top = grid[this.index(this.i, this.j - 1)];
+        let right = grid[this.index(this.i + 1, this.j)];
+        let bottom = grid[this.index(this.i, this.j + 1)];
+        let left = grid[this.index(this.i - 1, this.j)];
+
+        if (top && !top.visited){
+            neighbors.push(top);
+        }
+        if (right && !right.visited){
+            neighbors.push(right);
+        }
+        if (bottom && !bottom.visited){
+            neighbors.push(bottom);
+        }
+        if (left && !left.visited){
+            neighbors.push(left);
+        }
+
+        if (neighbors.length > 0) {
+            let r = floor(random(0, neighbors.length));
+            return neighbors[r];
+        } else {
+            return undefined;
+        }
+    }
+
+    highlight() {
+        var x = this.i * w;
+        var y = this.j * w;
+        noStroke();
+        fill(0, 255, 0, 100);
+        rect(x, y, w, w);
     }
 
     show() {
@@ -26,5 +71,11 @@ class Cell {
         if (this.walls[3]) {
             line(x, y+  w, x, y)
         }   
+
+        if (this.visited) {
+            noStroke();
+            fill(255, 0, 255, 100)
+            rect(x, y, w, w);
+        }
     }
 }
--- 7-maze-generator/sketch.js
+++ 7-maze-generator/sketch.js
@@ -1,24 +1,65 @@
 let cols, rows;
-let w = 40;
+let w = 20;
 let grid = [];
+
+let current;
+let stack = [];
 
 function setup() {
   createCanvas(400, 400);
   cols = floor(width/w);
   rows = floor(height/w);
+  // debug
+  frameRate(5);
 
   for(let j = 0; j < rows; j++) {
     for (let i = 0; i < cols; i++) {
       grid.push(new Cell(i,j));
     }
   }
+  current = grid[0];
 }
-
 
 function draw() {
   background(51);
   for (let i = 0; i < grid.length; i++) {
     grid[i].show();
   }
+  current.visited = true;
+  current.highlight();
+  // step 1
+  let next =  current.checkNeighbors();
+  if (next) {
+    next.visited = true;
+    // step 2
+    stack.push(current);
+    // step 3    
+    removeWalls(current, next);
+    // step 4
+    current = next;
+  // step 5
+  } else if (stack.length > 0) {
+    current = stack.pop();  
+  }
+
 }
- 
+
+function removeWalls(a, b) {
+  let x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+
+  let y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+}
