27f2995171def2861558b3a95e298410fca4d0a3
Stephen Mahon <stemahon@gmail.com>
2022-03-21 09:19:51 -0700

    init commit for fractals

---
+++ 12-fractal-tree/sketch.js
@@ -0,0 +1,31 @@
+let angle = 0;
+let slider;
+
+function setup() {
+  createCanvas(400, 400);
+  slider = createSlider(0, TWO_PI, PI/2, 0.01);
+}
+
+function draw() {
+  background(51);
+  let len = 100;
+  angle = slider.value();
+  stroke(255);
+  translate(200, height);
+  branch(len);
+}
+
+function branch(len) {
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if(len > 4) {
+    push()
+    rotate(angle);
+    branch(2*len/3);
+    pop();
+    push();
+    rotate(-angle);
+    branch(2*len/3);
+    pop();
+  } 
+}
